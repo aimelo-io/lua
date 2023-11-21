52c7a4fa25a7b55fe6a3b40b4d88a3a0c0f3785e
ewenlaz <ewenlaz@gmail.com>
2023-11-21 18:14:07 +0800

    1.0.5 fix test

--- test.js
+++ test.js
@@ -9,18 +9,12 @@ console.timeLog('aaaaa')
 const format = function(bytes) { 
     return (bytes/1024/1024).toFixed(2)+'MB'; 
 };
+example.doString(`result = 88;`);
+const r = example.doString(`result = true;`);
+console.log(r);
 
-setInterval(() => {
-    example.doString(`result = 88;`);
-    const r = example.doString(`result = true;`);
-    console.log(r);
-}, 1);
-
-setInterval(() => {
-    const mem = process.memoryUsage();
-    console.log('Process: heapTotal '+format(mem.heapTotal) + ' heapUsed ' + format(mem.heapUsed) + ' rss ' + format(mem.rss));
-}, 5000)
-
+const mem = process.memoryUsage();
+console.log('Process: heapTotal '+format(mem.heapTotal) + ' heapUsed ' + format(mem.heapUsed) + ' rss ' + format(mem.rss));
 
 console.timeLog('aaaaa')
 // Unchanged. It prints 19
